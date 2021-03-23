d726f520d17c4e0991d10155ccf3bd7cba1d9658
zhy <2252593269@qq.com>
2021-03-23 23:22:37 +0800

    粘包解决方案 MessageProtocol.java

---
+++ IO/src/main/java/com/c/io/aio/netty/protocoltcp/MyServerHandler.java
@@ -0,0 +1,24 @@
+package com.c.io.aio.netty.protocoltcp;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+/**
+ * 自定义的 Handler 需要继承 Netty 规定好的接口; 才能被 Netty 框架所关联
+ **/
+public class MyServerHandler extends SimpleChannelInboundHandler<MessageProtocol> {
+
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, MessageProtocol msg) throws Exception {
+        int len = msg.getLen();
+        byte[] content = msg.getContent();
+
+        String s = new String(content, StandardCharsets.UTF_8);
+        System.out.println(new Date(System.currentTimeMillis()) + "\t收到客户端" + ctx.channel().remoteAddress() + "发送的消息：" + s);
+    }
+
+}
