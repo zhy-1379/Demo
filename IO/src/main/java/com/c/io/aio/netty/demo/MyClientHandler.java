6157a13178348987a5206cefa16fa6546a147fd6
zhy <2252593269@qq.com>
2021-03-24 20:27:56 +0800

    加一些注释

--- IO/src/main/java/com/c/io/aio/netty/demo/MyClientHandler.java
+++ IO/src/main/java/com/c/io/aio/netty/demo/MyClientHandler.java
@@ -2,10 +2,18 @@ package com.c.io.aio.netty.demo;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelOutboundInvoker;
 import io.netty.util.CharsetUtil;
 
+/**
+ * 发送消息；使用 {@link ChannelOutboundInvoker#writeAndFlush(Object)} 接口
+ * <p>
+ * 在 {@link ChannelHandler#handlerAdded(ChannelHandlerContext)} 时保留
+ * {@link ChannelHandlerContext}，可以在之后使用：比如发送消息
+ */
 public class MyClientHandler extends ChannelInboundHandlerAdapter {
 
     private String message;
--- IO/src/main/java/com/c/io/aio/netty/protocoltcp/MessageProtocol.java
+++ IO/src/main/java/com/c/io/aio/netty/protocoltcp/MessageProtocol.java
@@ -1,7 +1,18 @@
 package com.c.io.aio.netty.protocoltcp;
 
 
-//协议包
+import io.netty.buffer.ByteBuf;
+
+/**
+ * 协议包；将消息长度和消息内容封装成一个对象
+ * <p>
+ * - 消息发送方 使用 Encoder 将一个 {@link MessageProtocol} 的 {@link MessageProtocol#len}
+ * 和 {@link MessageProtocol#content} 一起写入 {@link ByteBuf}
+ * <p>
+ * - 消息接收方 使用 Decoder 先读取 {@link ByteBuf#readInt()} 作为一个即将读取内容的长度 len，
+ * 再使用 {@link ByteBuf#readBytes(byte[])} )} 读取长度为 len 的字节数组；封装成一个协议包
+ * {@link MessageProtocol} 并写入消息列表
+ */
 public class MessageProtocol {
     private int len; //关键
     private byte[] content;
