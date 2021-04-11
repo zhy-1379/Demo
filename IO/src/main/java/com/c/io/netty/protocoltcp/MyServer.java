ce4c34f8afdd7219ae40eace0153ecddf891d11f
zhy <2252593269@qq.com>
2021-04-11 11:15:16 +0800

    修改内容：修改包名

---
+++ IO/src/main/java/com/c/io/netty/protocoltcp/MyServer.java
@@ -0,0 +1,65 @@
+package com.c.io.netty.protocoltcp;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class MyServer {
+    private int port;
+
+    public MyServer(int port) {
+        this.port = port;
+    }
+
+    public void run() throws InterruptedException {
+        //创建两个线程组 boosGroup、workerGroup
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            //创建服务端的启动对象，设置参数
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            //设置两个线程组boosGroup和workerGroup
+            bootstrap.group(bossGroup, workerGroup)
+                    //设置服务端通道实现类型
+                    .channel(NioServerSocketChannel.class)
+                    //设置线程队列得到连接个数
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    // .option(ChannelOption.SO_TIMEOUT, 0)
+                    //设置保持活动连接状态
+                    .childOption(ChannelOption.SO_KEEPALIVE, true)
+                    //使用匿名内部类的形式初始化通道对象
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            // 给pipeline管道设置处理器
+                            // ch.pipeline().addLast(new StringEncoder());
+                            // ch.pipeline().addLast(new StringDecoder());
+                            ch.pipeline().addLast(new MyMessageDecoder());
+                            ch.pipeline().addLast(new MyMessageEncoder());
+                            ch.pipeline().addLast(new MyServerHandler());
+                        }
+                    });//给workerGroup的EventLoop对应的管道设置处理器
+            System.out.println("服务端已经准备就绪...");
+            //绑定端口号，启动服务端
+            ChannelFuture channelFuture = bootstrap.bind(port).sync();
+            //对关闭通道进行监听
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = args.length > 1 ? Integer.parseInt(args[1]) : 7070;
+
+        new MyServer(port).run();
+    }
+
+}
